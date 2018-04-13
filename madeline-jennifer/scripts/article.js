30301ebcb6067a8bfd6b18134901264d4ead2a40
MADELINE PETERS <MADELINEPET@HOTMAIL.COM>
2018-04-13 09:35:58 -0700

    We used handlebars to generate the article content.

---
+++ madeline-jennifer/scripts/article.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let articles = [];
+
+function Article (rawDataObj) {
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.body = rawDataObj.body;
+  this.publishedOn = rawDataObj.publishedOn;
+}
+
+Article.prototype.toHtml = function() {
+  // REVIEW: If your template will use properties that aren't on the object yet, add them.
+  let template = $('#article-template').html();
+  let templateToRender = Handlebars.compile(template);
+
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? `Published ${this.daysAgo} Days Ago` : '(draft)';
+  return templateToRender(this);
+};
+
+// COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
+//Parentheses are needed when you have more than one argument. The other two functions only have one, making it optional to use parentheses.
+rawData.sort((a,b) => {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+rawData.forEach(articleObject => {
+  articles.push(new Article(articleObject));
+});
+
+articles.forEach(article => {
+  $('#articles').append(article.toHtml());
+});
